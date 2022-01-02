fa2651f2828dc7acc02800a455ace58f3702dbf3
JZ-FSDev <94236556+JZ-FSDev@users.noreply.github.com>
2022-01-02 11:44:52 -0600

    Update Day_7_Solution.java
    Separated main into partOne and partTwo methods for more readability.

--- Day 7/Day_7_Solution.java
+++ Day 7/Day_7_Solution.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Day_7_Solution {
+
     private static final String FILE = "Day_7_Input.txt";
+    private static int lowest = 0, highest = -1;
+    private static ArrayList<Integer> pos = new ArrayList<Integer>();
 
     /*
         Parses through input file to get initial crab positions then finds the most optimal position horizontally to
-        orient the crabs to ensure the lowest fuel spent.
+        orient the crabs to ensure the lowest fuel spent for each of the parts.
     */
     public static void main(String[] args) {
-        ArrayList<Integer> pos = new ArrayList<Integer>();
-
-        int lowest = 0, highest = -1;
 
         try {
             Scanner sc = new Scanner(new FileReader(FILE));
@@ -33,6 +32,30 @@ public class Day_7_Solution {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("Lowest Fuel Part One: " + partOne() + "  Lowest Fuel Part Two: " + partTwo());
+    }
+
+    /*
+        Returns the lowest fuel realative to each horizontal position consuming only one fuel
+    */
+    public static int partOne(){
+        int lowestFuel = Integer.MAX_VALUE;
+        for(int i = lowest; i < highest; i++){
+            int fuel = 0;
+            for(int j = 0; j < pos.size(); j++){
+                fuel += Math.abs(pos.get(j)-i);
+            }
+            if(fuel < lowestFuel){
+                lowestFuel = fuel;
+            }
+        }
+        return lowestFuel;
+    }
+
+    /*
+        Returns the lowest fuel realative to each horizontal position consuming one fuel more than the previous
+    */
+    public static int partTwo(){
         int lowestFuel = Integer.MAX_VALUE;
         for(int i = lowest; i < highest; i++){
             int fuel = 0;
@@ -46,6 +69,6 @@ public class Day_7_Solution {
                 lowestFuel = fuel;
             }
         }
-        System.out.println("Lowest Fuel: " + lowestFuel);
+        return lowestFuel;
     }
 }
